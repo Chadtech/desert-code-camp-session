b53b026341bf29ab86c1cf1875f1984d3785ff23
Chadtech <chadstearns1@gmail.com>
2016-10-04 16:03:50 -0700

    Fixed some of my language

--- src/i-merging.js
+++ src/i-merging.js
@@ -7,29 +7,29 @@ Old Vdom          New Vdom
 
 diff(div, div);
   div == div
-  -> Same! No change necessary
+  -> Theyre the same, so no change necessary
 
   diff(div.children[0], div.children[0]);
     p == p 
-    -> Same! No Change necessary
+    -> Theyre the same, so no Change necessary
     diff(p.children[0], p.children[0])
       "cool" == "cool"
-      -> Same! No Change necessary
+      -> Theyre the same, so no Change necessary
 
   diff(div.children[1], div.children[1]);
     p == p 
-    -> Same! No Change necessary
+    -> Theyre the same, so no Change necessary
     diff(p.children[0], p.children[0])
       "dog" == "cat"
-      -> Different! Lets fix it
+      -> Theyre difference, so lets fix it
       merge("dog", "cat");
         -> Remove "dog"
         -> Add "Cat"
 
   diff(div.children[2], div.children[2]);
     p == undefined
-    -> Different! Lets fix it
+    -> Theyre different, so lets fix it
     merge(p, undefined)
       -> Add p "man"
-      
+
 
