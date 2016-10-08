e43f65c76d4d6afa26a794927cdf6844ff4db840
Chadtech <chadstearns1@gmail.com>
2016-10-08 00:55:28 -0700

    Added just one little bit to the 0th file

--- src/0-div.js
+++ src/0-div.js
@@ -1,5 +1,13 @@
+
+function element(tag, attributes, children) {
+
+  // Whatever business
+
+}
+
 function div() {
   return document.createElement("div");
 };
 
 document.body.appendChild( div() );
+
